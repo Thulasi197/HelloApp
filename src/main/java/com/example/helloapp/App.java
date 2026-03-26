ce0bf4f745b3d9a40367d97d2586a613c36c2e02
thulasi <thulasi195v@gmail.com>
2026-03-26 15:49:27 +0530

    UC1 implemeted

---
+++ src/main/java/com/example/helloapp/App.java
@@ -0,0 +1,12 @@
+package com.example.helloapp;
+/**
+ * HelloApp - UC1 - Display Hello World
+ */
+public class App {
+    public static void main(String[] args) {
+        
+        // Display Hello World to console
+        System.out.println("Hello World");
+        
+    }
+}
