7edd5f9e9360303992ed4d3199e0159d55435520
thulasi <thulasi195v@gmail.com>
2026-04-02 07:25:52 +0530

    feature/UC5 implemented

--- src/main/java/com/example/helloapp/App.java
+++ src/main/java/com/example/helloapp/App.java
@@ -1,17 +1,16 @@
 package com.example.helloapp;
 import java.util.*;
 public class App {
-	    public static void main(String[] args) {
+    public static void main(String[] args) {
 
-	        // If no arguments are passed
-	        if (args.length == 0) {
-	            System.out.println("Hello, World!");
-	        } else {
-	            // Loop through all names
-	            for (String name : args) {
-	                System.out.println("Hello, " + name + "!");
-	            }
-	        }
-	    }
-	}
-    
+        // If no arguments are provided
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            // Enhanced for-loop (for-each)
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
+        }
+    }
+}
